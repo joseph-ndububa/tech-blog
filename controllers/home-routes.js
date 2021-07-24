fb7f9db54fa47e0e3991152aceb9807651feeeeb
josephn90 <49831070+josephn90@users.noreply.github.com>
2021-07-24 13:09:43 -0400

    dashboard get all posts

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -85,3 +85,13 @@ router.get('/post/:id', (req, res) => {
             res.status(500).json(err);
         });
 });
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+});
+
+module.exports = router;
