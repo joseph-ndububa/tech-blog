b334a47726017470365ddc70ce0b41a022dd703b
josephn90 <49831070+josephn90@users.noreply.github.com>
2021-07-24 13:42:26 -0400

    login route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -66,3 +66,31 @@ router.post('/', (req, res) => {
             res.status(500).json(err);
         });
 });
+
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            username: req.body.username
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'User does not exist.' });
+            return;
+        }
+
+        const validPassword = dbUserData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password.' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, message: 'You are now logged in!' });
+        });
+    });
+});
