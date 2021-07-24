a9de6812389c52df19d361b2633aeda5424398be
josephn90 <49831070+josephn90@users.noreply.github.com>
2021-07-24 13:49:49 -0400

    delete a user

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -135,3 +135,26 @@ router.put('/:id', (req, res) => {
             res.status(500).json(err);
         });
 });
+
+// delete a user
+
+router.delete('/:id', (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'User not found' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
