f5aeb085e13be2c758c43c04f2ae8657004f10a5
josephn90 <49831070+josephn90@users.noreply.github.com>
2021-07-24 13:38:24 -0400

    get one user

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -12,3 +12,37 @@ router.get('/', (req, res) => {
             res.status(500).json(err);
         });
 });
+
+router.get('/:id', (req, res) => {
+    User.findOne({
+        attributes: { exclude: ['password'] },
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'title', 'post_url', 'created_at']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'created_at'],
+                include: {
+                    model: Post,
+                    attributes: ['title']
+                }
+            }
+        ]
+    })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'User not found.' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
