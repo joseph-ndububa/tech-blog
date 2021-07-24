5e45b6cf89dee4ec0ffaf519709c3817ec6ea830
josephn90 <49831070+josephn90@users.noreply.github.com>
2021-07-24 13:18:30 -0400

    delete a comment

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -23,3 +23,24 @@ router.post('/', withAuth, (req, res) => {
             res.status(400).json(err);
         });
 });
+
+router.delete('/:id', withAuth, (req, res) => {
+    Comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbCommentData => {
+            if (!dbCommentData) {
+                res.status(404).json({ message: 'Comment not found.' });
+                return;
+            }
+            res.json(dbCommentData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
