b142c0b3fb726e8b56d498ba5c53b48a19604022
josephn90 <49831070+josephn90@users.noreply.github.com>
2021-07-24 13:14:57 -0400

    api router

---
+++ controllers/api/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const commentRoutes = require('./comment-routes');
+const postRoutes = require('./post-routes');
+const userRoutes = require('./user-routes.js');
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
+
+module.exports = router;
