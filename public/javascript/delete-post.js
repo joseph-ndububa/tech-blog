6c389d363ab81a8fb77d64b274bac76f75f7e2a5
josephn90 <49831070+josephn90@users.noreply.github.com>
2021-07-24 15:09:23 -0400

    logic for deleting posts

---
+++ public/javascript/delete-post.js
@@ -0,0 +1,17 @@
+async function deletePostHandler(event) {
+    event.preventDefault();
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+    const res = await fetch(`/api/posts/${id}`, {
+        method: 'DELETE'
+    });
+
+    if (res.ok) {
+        document.location.replace('/dashboard/');
+    } else {
+        alert(res.statusText);
+    }
+}
+
+document.querySelector('.delete-post-btn').addEventListener('click', deletePostHandler);
