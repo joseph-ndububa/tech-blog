6e40994a5ce011e0b2f4bee1cc5d7ca6eda3e4b0
josephn90 <49831070+josephn90@users.noreply.github.com>
2021-07-24 13:48:57 -0400

    update a user

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -13,6 +13,8 @@ router.get('/', (req, res) => {
         });
 });
 
+// get one user
+
 router.get('/:id', (req, res) => {
     User.findOne({
         attributes: { exclude: ['password'] },
@@ -47,6 +49,8 @@ router.get('/:id', (req, res) => {
         });
 });
 
+// add a user
+
 router.post('/', (req, res) => {
     User.create({
         username: req.body.username,
@@ -66,6 +70,8 @@ router.post('/', (req, res) => {
             res.status(500).json(err);
         });
 });
+
+// login
 
 router.post('/login', (req, res) => {
     User.findOne({
@@ -94,6 +100,8 @@ router.post('/login', (req, res) => {
     });
 });
 
+// logout
+
 router.post('/logout', (req, res) => {
     if (req.session.loggedIn) {
         req.session.destroy(() => {
@@ -103,4 +111,27 @@ router.post('/logout', (req, res) => {
     else {
         res.status(404).end();
     }
+});
+
+// update a user
+
+router.put('/:id', (req, res) => {
+
+    User.update(req.body, {
+        individualHooks: true,
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'User not found' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
